57cb96ab11902cf09fc35129faa212b73efa8995
so-dipe <sodipepaul@gmail.com>
2023-10-21 09:28:33 +0100

    icon

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const projects = document.querySelectorAll('.project');
     projects.forEach(project => {
-        project.addEventListener('click', function(event) {
-            const details = this.querySelector('.details');
-            const icon = this.querySelector('i');
-            if (!event.target.classList.contains('dropdown-icon')) {
-                details.style.display = details.style.display === 'none' ? 'block' : 'none';
-                if (icon.classList.contains('fa-chevron-down')) {
-                    icon.classList.remove('fa-chevron-down');
-                    icon.classList.add('fa-minus');
-                } else {
-                    icon.classList.remove('fa-minus');
-                    icon.classList.add('fa-chevron-down');
-                }
+        const projectTitle = project.querySelector('.project-title');
+        const icon = projectTitle.querySelector('i');
+        projectTitle.addEventListener('click', function(event) {
+            const details = project.querySelector('.details');
+            details.style.display = details.style.display === 'none' ? 'block' : 'none';
+            if (icon.classList.contains('fa-chevron-down')) {
+                icon.classList.remove('fa-chevron-down');
+                icon.classList.add('fa-minus');
+            } else {
+                icon.classList.remove('fa-minus');
+                icon.classList.add('fa-chevron-down');
             }
         });
     });
 });
+
+
+
